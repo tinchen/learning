3f04384aafd04be0f072eb4bc8d47633679c6602
tinchen <tin.chen@jii.tw>
2018-04-27 11:30:35 +0800

    better-scroll 應用

--- vue-js/sell-app/src/components/goods/Goods.vue
+++ vue-js/sell-app/src/components/goods/Goods.vue
@@ -2,13 +2,14 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuScroll">
       <ul>
-        <li class="menu-item">
+        <li class="menu-item" :class="{'current': currentIndex == 0}" @click="menuSelected(0)">
           <p class="text">
             <img class="icon" :src="container.tag_icon" v-if="container.tag_icon" />
             {{ container.tag_name }}
           </p>
         </li>
-        <li class="menu-item" v-for="item in goods" :key="item.idx">
+        <li class="menu-item" v-for="(item, idx) in goods" :key="item.idx"
+            :class="{'current': currentIndex == (idx+1)}" @click="menuSelected(idx+1)">
           <p class="text">
             <img class="icon" :src="item.icon" v-if="item.icon" />
             {{ item.name }}
@@ -18,13 +19,13 @@
     </div>
     <div class="foods-wrapper" ref="foodsScroll">
       <ul>
-        <li class="container-list">
+        <li class="container-list food-list-hook">
           <div v-for="item in container.operation_source_list" :key="item.idx">
             <img :src="item.pic_url" alt="">
           </div>
         </li>
 
-        <li class="food-list" v-for="item in goods" :key="item.idx">
+        <li class="food-list food-list-hook" v-for="item in goods" :key="item.idx">
           <h3 class="title">{{ item.name }}</h3>
           <ul>
             <li class="food-item" v-for="food in item.spus" :key="food.idx">
@@ -58,7 +59,11 @@ export default {
   data() {
     return {
       container: {},
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0,
+      menuScroll: null,
+      foodsScroll: null
     }
   },
   created() {
@@ -72,6 +77,7 @@ export default {
         //console.log(that.goods);
         that.$nextTick(() => {
           that.initScroll();
+          that.caculateHeight();
         });
       }
     }).catch(function (err) {
@@ -86,8 +92,41 @@ export default {
       return style;
     },
     initScroll() {
-      new BScroll(this.$refs.menuScroll);
-      new BScroll(this.$refs.foodsScroll);
+      this.menuScroll = new BScroll(this.$refs.menuScroll, { click: true});
+      this.foodsScroll = new BScroll(this.$refs.foodsScroll, { probeType: 3});
+
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+    },
+    caculateHeight() {
+      let foodList = this.$refs.foodsScroll.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i=0; i<foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    },
+    menuSelected(idx) {
+      //console.log(idx);
+      let foodList = this.$refs.foodsScroll.getElementsByClassName('food-list-hook');
+      let el = foodList[idx];
+      this.foodsScroll.scrollToElement(el, 250);
+    }
+  },
+  computed: {
+    currentIndex() {
+      for (let i=0; i<this.listHeight.length; i++) {
+        let h1 = this.listHeight[i];
+        let h2 = this.listHeight[i+1];
+        if (!h2 || (this.scrollY >= h1 && this.scrollY < h2)) {
+          //console.log(i);
+          return i;
+        }
+      }
+      return 0;
     }
   }
 }
